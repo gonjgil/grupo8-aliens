9b9adfaeb0fe4adc0db2216fb00b67e4da17faea
flors <florsolodujin@gmail.com>
2025-10-06 02:05:38 -0300

    correcciones en las capas del perfil del artista. Agregué estilos al formulario de creación de artista

---
+++ src/main/webapp/resources/core/js/perfil_artista.js
@@ -0,0 +1,17 @@
+const BTN_EDITAR = document.getElementById("btn-editar");
+const NOMBRE = document.getElementById("nombre-artista");
+const BIO = document.getElementById("bio-artista");
+
+// Función para simular edición
+function habilitarEdicion() {
+  const NUEVO_NOMBRE = prompt("Editar nombre:", NOMBRE.innerText);
+  const NUEVA_BIO = prompt("Editar biografía:", BIO.innerText);
+
+  if (NUEVO_NOMBRE) {
+    NOMBRE.innerText = NUEVO_NOMBRE;
+  }
+  if (NUEVA_BIO) {
+    BIO.innerText = NUEVA_BIO;
+  }
+}
+BTN_EDITAR.addEventListener("click", habilitarEdicion);
