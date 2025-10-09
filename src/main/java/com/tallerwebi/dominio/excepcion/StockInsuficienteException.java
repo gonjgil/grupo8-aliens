e5bd498c9a054678e70d95f08209f2d634d3b581
SolSantillan-ss <santillansol.ss@gmail.com>
2025-10-09 03:14:52 -0300

    sin cambios

--- src/main/java/com/tallerwebi/dominio/excepcion/StockInsuficienteException.java
+++
@@ -1,8 +0,0 @@
-package com.tallerwebi.dominio.excepcion;
-
-public class StockInsuficienteException extends Exception {
-
-    public StockInsuficienteException(){
-        super("Stock insuficiente");
-    }
-}
