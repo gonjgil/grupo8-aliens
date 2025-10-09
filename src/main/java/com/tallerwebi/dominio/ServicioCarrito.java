d012064f224a24c203700b3bc908eb117584f772
SolSantillan-ss <santillansol.ss@gmail.com>
2025-10-09 01:02:25 -0300

    servioCarritoImpl

--- src/main/java/com/tallerwebi/dominio/ServicioCarrito.java
+++ src/main/java/com/tallerwebi/dominio/ServicioCarrito.java
@@ -2,13 +2,18 @@ package com.tallerwebi.dominio;
 
 import com.tallerwebi.dominio.excepcion.NoExisteLaObra;
 import com.tallerwebi.dominio.excepcion.CarritoVacioException;
+import com.tallerwebi.dominio.excepcion.StockInsuficienteException;
+import com.tallerwebi.presentacion.ObraDto;
+
+import java.util.List;
 
 public interface ServicioCarrito {
     Carrito obtenerOCrearCarritoParaUsuario(Usuario usuario);
-    void agregarObraAlCarrito(Usuario usuario, Long obraId, Integer cantidad) throws NoExisteLaObra;
+    void agregarObraAlCarrito(Usuario usuario, Long obraId) throws NoExisteLaObra, StockInsuficienteException;
     void removerObraDelCarrito(Usuario usuario, Long obraId);
     void actualizarCantidadObra(Usuario usuario, Long obraId, Integer nuevaCantidad);
     void limpiarCarrito(Usuario usuario);
+    List<ObraDto> obtenerObras();
     Carrito obtenerCarritoConItems(Usuario usuario);
     Double calcularTotalCarrito(Usuario usuario);
     Integer contarItemsEnCarrito(Usuario usuario);
