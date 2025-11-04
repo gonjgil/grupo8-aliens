6160323f92fc2e76c02fc4a99b3cb2ebe9ccf406
SolSantillan-ss <santillansol.ss@gmail.com>
2025-11-04 02:13:55 -0300

    modificacion de MercadoPagoConfig. necesaria

--- src/main/java/com/tallerwebi/config/MercadoPagoConfig.java
+++ src/main/java/com/tallerwebi/config/MercadoPagoConfig.java
@@ -1,12 +1,32 @@
 package com.tallerwebi.config;
 
+import com.mercadopago.client.payment.PaymentClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MercadoPagoConfig {
     
     // credenciales de MercadoPago TEST harcodeadas para desarrollo, ver como inyectarlas en produccion con Spring
     private static final String ACCESS_TOKEN = "TEST-6153218614070129-100119-44856ce364468a90fdd1390c950cfb90-97406022";
     private static final String PUBLIC_KEY = "TEST-12b898d1-056f-4353-9d79-d6cfa383e217";
     private static final String BASE_URL = "http://localhost:8080/spring";
-    
+
+
+    static {
+        // Inicializa el SDK global con el access token
+        MercadoPagoConfig.setAccessToken(ACCESS_TOKEN);
+    }
+
+    private static void setAccessToken(String accessToken) {
+    }
+
+    @Bean
+    public PaymentClient paymentClient() {
+        return new PaymentClient();
+    }
+
+
     public static String getAccessToken() {
         return ACCESS_TOKEN;
     }
