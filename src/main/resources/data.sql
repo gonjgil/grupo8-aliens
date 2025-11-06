c795a8d9ec925b9211a431812324d06d749348bd
Gabii-To <gabriel.torrico@yahoo.com>
2025-11-06 16:55:39 -0300

    Cambio de Vistas (parcial)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,19 @@ WHERE NOT EXISTS (
     SELECT 1 FROM Usuario WHERE email = 'test@unlam.edu.ar'
 );
 
+-- Cambiar el charset y collation de la base de datos actual a utf8
+ALTER DATABASE tallerwebi CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Configurar la conexión actual a UTF-8 (por seguridad)
+SET NAMES utf8mb4;
+SET CHARACTER SET utf8mb4;
+SET collation_connection = 'utf8mb4_unicode_ci';
+
+-- Cambia las tablas actuales a utf8
+SELECT CONCAT('ALTER TABLE `', table_name, '` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;')
+FROM information_schema.tables
+WHERE table_schema = 'tallerwebi';
+
 -- Insertar artistas
 INSERT INTO Artista (id, nombre, biografia) VALUES
 (1, 'J. Doe', 'Artista contemporáneo especializado en paisajismo y retratos modernos.'),
