4a90b8dfe5f0039f5457db012dbc4842371befc0
Gabii-To <gabriel.torrico@yahoo.com>
2025-11-20 16:59:38 -0300

    Finalizar Compra

--- src/main/java/com/tallerwebi/config/MercadoPagoConfig.java
+++ src/main/java/com/tallerwebi/config/MercadoPagoConfig.java
@@ -10,7 +10,7 @@ public class MercadoPagoConfig {
     // credenciales de MercadoPago TEST harcodeadas para desarrollo, ver como inyectarlas en produccion con Spring
     private static final String ACCESS_TOKEN = "APP_USR-1109986656199883-111215-c876508d0dd62b9f2e4c8014e4a2987d-2985296777";
     private static final String PUBLIC_KEY = "APP_USR-c4bbba92-da84-4086-a168-c07b37cf879b";
-    private static final String BASE_URL = "https://elsie-breechless-adrianne.ngrok-free.dev/spring";
+    private static final String BASE_URL = "https://sheree-unawed-nonpreferably.ngrok-free.dev/spring";
 
 
     static {
